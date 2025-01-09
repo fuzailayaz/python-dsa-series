f781f2c345bac14a8d6373d03965af0b35914e9f
MD Fuzail Ayaz <101309593+fuzailayaz@users.noreply.github.com>
2025-01-09 14:17:55 +0530

    Added comments to this problem

--- minimum_size_subarray_sum.py
+++ minimum_size_subarray_sum.py
@@ -2,14 +2,18 @@ class solution:
     def minSubArrayLen(self, target:int, nums:list[int]) -> int:
         l, total = 0, 0
         res = float("inf")
-        for r in range(len(nums)):
+        for r in range(len(nums)): # in this line: 
+            # r is auto incremented through the loop 
+            # and not have to do like r += 1 as og below l+=1
             total += nums[r]
             while total >= target:
                 res = min(r - l + 1, res)
                 total -= nums[l]
-                l += 1
+                l += 1 
 
         return 0 if res == float("inf") else res
-sol = solution()
+sol = solution() # object/instance created in order to 
+                 # access the function inside the 
+                 # class to get the result and o/p for the function
 result = sol.minSubArrayLen(7,[2,3,1,2,4,3])
 print(result)
