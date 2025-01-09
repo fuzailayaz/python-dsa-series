ae476e8deaf23aa34294a735d489b9a693a71d4d
MD Fuzail Ayaz <101309593+fuzailayaz@users.noreply.github.com>
2025-01-09 23:49:14 +0530

    updated without initailizing constructor

--- move_zeroes.py
+++ move_zeroes.py
@@ -1,15 +1,12 @@
 class MoveZeros:
-    def __init__(self,nums):
-        self.nums = nums
-    def move_zeros(self):
+    def move_zeros(self, nums):
         l, r = 0, 0
-        while r < len(self.nums):
-            if self.nums[r] != 0:
-                self.nums[l], self.nums[r] = self.nums[r], self.nums[l]
+        while r < len(nums):
+            if nums[r] != 0:
+                nums[l], nums[r] = nums[r], nums[l]
                 l += 1
             r += 1
-        return self.nums
-if __name__ == '__main__':
-    nums = [0,1,0,3,12]
-    move_zeros = MoveZeros(nums)
-    print(move_zeros.move_zeros())
+        return nums
+nums = [0, 1, 0, 3, 12]
+sol = MoveZeros()
+print(sol.move_zeros(nums))
