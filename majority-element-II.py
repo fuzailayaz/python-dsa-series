74bc5d79125da8bf1b84ce89e33c72cc793b20f5
MD Fuzail Ayaz <101309593+fuzailayaz@users.noreply.github.com>
2025-01-12 23:56:41 +0530

    Commited Majority-Element-II problem Solution

---
+++ majority-element-II.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+
+class Solution:
+    def majorityElement(self,nums:list[int]) -> list[int]:
+        count = defaultdict(int)
+        for n in nums:
+            count[n] += 1
+
+            if len(count) <= 2:
+                continue
+            new_count = defaultdict(int)
+            for n, c in count.items():
+                if c > 1:
+                    new_count[n] = c - 1
+            count = new_count
+
+        res = []
+        for n in count:
+            if nums.count(n) > len(nums) // 3:
+                res.append(n)
+        return res
+sol = Solution()
+nums = [2,3,2]
+print(sol.majorityElement(nums))
