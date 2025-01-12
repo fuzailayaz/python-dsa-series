8f9ba3a46620806ceaeaa879fe9be5fc2582d1c0
MD Fuzail Ayaz <101309593+fuzailayaz@users.noreply.github.com>
2025-01-12 23:36:06 +0530

    Commited alternative way for majority-element question with fewer lines of codes below the commented part

--- majority-element.py
+++ majority-element.py
@@ -1,15 +1,27 @@
-class Solution:
-    def majorityElement(self, nums:list[int]) -> int:
-        ans, count = -1, 0
+'''class Solution:
+    def majorityElement(self,nums:list[int]) -> int:
+        ans,count  = -1,0
         for num in nums:
             if count == 0:
                 ans = num
-
-            if ans == num:
-                count += 1
-            else:
+            if ans == count:
                 count -= 1
+            else:
+                count += 1
         return ans
+sol = Solution()
+nums = [2,2,1,1,1,2,2]
+print(sol.majorityElement(nums)) '''
+
+class Solution:
+    def majorityElement(self,nums:list[int]) -> int:
+        res, count = 0,0
+
+        for n in nums:
+            if count == 0:
+                res = n
+            count += (1 if n == res else -1)
+        return res
 sol = Solution()
 nums = [2,2,1,1,1,2,2]
 print(sol.majorityElement(nums))
