67a64f606f9ea9545a96f95ddf1d72734adeeadb
MD Fuzail Ayaz <101309593+fuzailayaz@users.noreply.github.com>
2025-01-26 16:08:09 +0000

    26-01-25

--- find-leaders.py
+++ find-leaders.py
@@ -1,19 +1,17 @@
 class Solution:
     def findleaders(self,arr):
         n = len(arr)
-        leaders = []
-        max_from_right = arr[-1]
-        # add/append the last elements of the array to the leaders list
-        leaders.append(max_from_right)
+        max_from_right = arr[n-1]
+        leaders = [max_from_right]
 
         # Now traverse the element from right side of the array
         for i in range(n-2, -1, -1): # Start,stop & step
-            if arr[i] > max_from_right:
+            if arr[i] >= max_from_right:
                 max_from_right = arr[i]
-                leaders.append(max_from_right)
-        # Now since the list is in reverse order as per our
-        #  coding logic so we will reverse again in order to get the correct list
+                leaders.append(arr[i])
+        # Now since the list is in reverse order as per our coding logic
+        # so we will reverse again in order to get the correct list
         return leaders[::-1]
 sol = Solution()
-arr = [16,17,4,3,5,2]
+arr = [61,61,17]
 print(f"leaders of the array are elements in the array: {sol.findleaders(arr)}")
--- majority-element-II.py
+++ majority-element-II.py
@@ -1,6 +1,5 @@
 from collections import defaultdict
 
-
 class Solution:
     def majorityElement(self,nums:list[int]) -> list[int]:
         count = defaultdict(int)
