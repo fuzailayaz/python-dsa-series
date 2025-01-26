04526407340dd2381fb7f04616cb6fac1873b34d
MD Fuzail Ayaz <101309593+fuzailayaz@users.noreply.github.com>
2025-01-26 16:52:31 +0000

    Commited Binary Search Concept

---
+++ binary-search.py
@@ -0,0 +1,26 @@
+class BinarySearch:
+    def search(self, arr, target):
+        '''
+            Binary Search works on sorted arrays,   
+        Paramenters:
+            arr -> Sorted List in ascending order
+            target -> Element which needs to be find/seached in the array
+        Returns:
+            Target Index else ->  -1
+        '''
+        left = 0
+        right = len(arr) - 1
+
+        while left <= right:
+            # First Calculate mid # In order to avoid overflow condition
+            mid = right - (right - left) // 2 
+            if arr[mid] == target:
+                return mid
+            if arr[mid] < target:
+                left =  mid + 1 # Target is in right - half
+            else:
+                right = mid - 1 # Target is in left - half
+        return -1
+sol = BinarySearch()
+arr, target = [2,4,6,8,10,12,14], 10
+print(sol.search(arr,target))
