8bc2a18a278b083e7965a0e33365f0646c437736
MD Fuzail Ayaz <101309593+fuzailayaz@users.noreply.github.com>
2025-01-28 15:02:58 +0000

    Added comments to find occurances in sorted array problem

--- count-occurances.py
+++ count-occurances.py
@@ -1,7 +1,7 @@
 class Solution:
     def countFreq(self, arr, target):
         left, right, result = 0, len(arr) - 1, -1
-        
+        # result is used to store the last index at which target is found
         while left <= right:
             mid = left + (right - left) // 2
 
@@ -15,7 +15,10 @@ class Solution:
         if result == -1:
             return 0
         
-        first = result
+        first = result # first is used to store the index of 
+        # the first occurance at which target is found
+
+        #  to check the the remaining left portion after the first occurance 
         while first > 0 and arr[first - 1] == target:
             first -= 1
         return result - first + 1
